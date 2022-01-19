48323436d4bb28311d06b31a1059e9e781a599f9
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-01-19 12:28:35 +0100

    Routing management

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Post from './components/posts/Post'
+import Posts from './components/posts/Posts'
+import Account from './components/users/Account'
+import Login from './components/users/Login'
+import Profile from './components/users/Profile'
+import Register from './components/users/Register'
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <HashRouter>
+          <div className="container-fluid">
+            <Switch>
+              <Route exact path='/' component={Homepage}/>
+              <Route exact path='/posts' component={Posts}/>
+              <Route exact path='/post:id' component={Post}/>
+              <Route exact path='/account' component={Account}/>
+              <Route exact path='/register' component={Register}/>
+              <Route exact path='/login' component={Login}/>
+            </Switch>
+          </div>
+      </HashRouter>
   );
 }
 
