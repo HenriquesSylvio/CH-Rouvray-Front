43b67be000996e8713edc9d601fab4d1b0871f77
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-01-20 10:18:45 +0100

    creation of the design of detail post (not final)

--- src/pages/posts/Post.jsx
+++ src/pages/posts/Post.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Post = (props) => {
-    return <h1>Post</h1>
+    return (
+        <div className="card border-primary mb-3">
+            <h1 className="display-3">Post name</h1>
+            <p className="lead">Créé à : .... | Créé par : ....</p>
+            <hr className="my-1"/>
+            <p>Post Content </p>
+        </div>
+    );
 };
 
 export  default Post;
--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -3,73 +3,74 @@ import React from 'react';
 const Posts = (props) => {
     return (
         <div className="card-container">
-            <div className="card border-primary mb-3">
+            <br/>
+            <div className="card border-primary mb-2">
                 <div className="card-header">Post name</div>
                 <div className="card-body">
                     <p className="card-text">Post Content</p>
                 </div>
             </div>
-            <div className="card border-primary mb-3">
+            <div className="card border-primary mb-2">
                 <div className="card-header">Post name</div>
                 <div className="card-body">
                     <p className="card-text">Post Content</p>
                 </div>
             </div>
-            <div className="card border-primary mb-3">
+            <div className="card border-primary mb-2">
                 <div className="card-header">Post name</div>
                 <div className="card-body">
                     <p className="card-text">Post Content</p>
                 </div>
             </div>
-            <div className="card border-primary mb-3">
+            <div className="card border-primary mb-2">
                 <div className="card-header">Post name</div>
                 <div className="card-body">
                     <p className="card-text">Post Content</p>
                 </div>
             </div>
-            <div className="card border-primary mb-3">
+            <div className="card border-primary mb-2">
                 <div className="card-header">Post name</div>
                 <div className="card-body">
                     <p className="card-text">Post Content</p>
                 </div>
             </div>
-            <div className="card border-primary mb-3">
+            <div className="card border-primary mb-2">
                 <div className="card-header">Post name</div>
                 <div className="card-body">
                     <p className="card-text">Post Content</p>
                 </div>
             </div>
-            <div className="card border-primary mb-3">
+            <div className="card border-primary mb-2">
                 <div className="card-header">Post name</div>
                 <div className="card-body">
                     <p className="card-text">Post Content</p>
                 </div>
             </div>
-            <div className="card border-primary mb-3">
+            <div className="card border-primary mb-2">
                 <div className="card-header">Post name</div>
                 <div className="card-body">
                     <p className="card-text">Post Content</p>
                 </div>
             </div>
-            <div className="card border-primary mb-3">
+            <div className="card border-primary mb-2">
                 <div className="card-header">Post name</div>
                 <div className="card-body">
                     <p className="card-text">Post Content</p>
                 </div>
             </div>
-            <div className="card border-primary mb-3">
+            <div className="card border-primary mb-2">
                 <div className="card-header">Post name</div>
                 <div className="card-body">
                     <p className="card-text">Post Content</p>
                 </div>
             </div>
-            <div className="card border-primary mb-3">
+            <div className="card border-primary mb-2">
                 <div className="card-header">Post name</div>
                 <div className="card-body">
                     <p className="card-text">Post Content</p>
                 </div>
             </div>
-            <div className="card border-primary mb-3">
+            <div className="card border-primary mb-2">
                 <div className="card-header">Post name</div>
                 <div className="card-body">
                     <p className="card-text">Post Content</p>
