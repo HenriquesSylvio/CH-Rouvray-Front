aa7ae58f16ae6e114753e5db512f01cabadc294a
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-01-20 12:14:54 +0100

    creation of the design of register page (not final)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarColor01">
                     <ul className="navbar-nav me-auto">
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/">
+                            <NavLink className="nav-link active" to="/">
                                 Accueil
                             </NavLink>
                         </li>
@@ -49,21 +49,6 @@ const Navbar = () => {
                 </div>
             </div>
         </nav>
-
-
-        // <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        //     <div className="container-fluid">
-        //         <a className="navbar-brand" href="#">Navbar</a>
-        //         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-        //                 data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false"
-        //                 aria-label="Toggle navigation">
-        //             <span className="navbar-toggler-icon"></span>
-        //         </button>
-        //
-        //     </div>
-        // </nav>
-
-
 )
 }
 
--- src/pages/users/Register.jsx
+++ src/pages/users/Register.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
 
 const Register = (props) => {
-    return <h1>Register</h1>
+    return (
+            <div className="card border-primary mt-3">
+                <legend>Création de votre compte</legend>
+                <div>
+                    <label>Prénom :
+                        <input
+                            type="text"
+                            name="first_name"
+                            className="form-control"
+                            id="first_name"
+                        />
+                    </label>
+                    <label>Nom :
+                        <input
+                            type="text"
+                            name="last_name"
+                            className="form-control"
+                            id="last_name"
+                        />
+                    </label>
+                </div>
+                <div className="form-group my-2">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="text"
+                        name="username"
+                        className="form-control"
+                        id="email"
+                        // placeholder="mail@mail.fr"
+                    />
+                </div>
+                <div className="form-group my-2">
+                    <label htmlFor="password">Mot de passe</label>
+                    <input
+                        type="password"
+                        name="password"
+                        className="form-control"
+                        id="password"
+                        // placeholder="Mot de passe"
+                    />
+                </div>
+                <button type="submit" className="btn btn-outline-primary">
+                    Se connecter
+                </button>
+            </div>
+    );
 };
 
 export  default Register;
