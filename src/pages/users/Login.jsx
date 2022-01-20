e37dbd7b19eeb4643997ae8dc8c5fbdd8683bda8
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-01-20 10:21:44 +0100

    creation of the design of login page (not final)

--- src/pages/users/Login.jsx
+++ src/pages/users/Login.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
 const Login = (props) => {
-    return <h1>Login</h1>
+    return (
+        <div className="tab-content">
+            <form className="form-profile">
+                <fieldset>
+                    <legend>Se connecter</legend>
+                    <div className="form-group my-2">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="text"
+                            name="username"
+                            className="form-control"
+                            id="email"
+                            placeholder="mail@mail.fr"
+                        />
+                    </div>
+                    <div className="form-group my-2">
+                        <label htmlFor="password">Mot de passe</label>
+                        <input
+                            type="password"
+                            name="password"
+                            className="form-control"
+                            id="password"
+                            placeholder="Mot de passe"
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-outline-primary">
+                        Se connecter
+                    </button>
+                </fieldset>
+            </form>
+        </div>
+    );
 };
 
 export  default Login;
