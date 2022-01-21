c6da3e62cfa63896344b4dad5928e0e5e7ce71a1
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-01-21 09:47:40 +0100

    centered fieldset

--- src/pages/users/Register.jsx
+++ src/pages/users/Register.jsx
@@ -2,52 +2,50 @@ import React from 'react';
 
 const Register = (props) => {
     return (
-            <div className="tab-content mt-3">
+            <div className="tab-content">
                 <form className="form-profile">
-                    <fieldset>
-                        <legend>Création de votre compte</legend>
-                        <div>
-                            <label>Prénom :
+                        <fieldset>
+                            <legend>Création de votre compte</legend>
+                            <div className="form-group my-2">
+                                <label>Prénom :</label>
                                 <input
                                     type="text"
                                     name="first_name"
                                     className="form-control"
                                     id="first_name"
                                 />
-                            </label>
-                            <label>Nom :
+                                <label>Nom :</label>
                                 <input
                                     type="text"
                                     name="last_name"
                                     className="form-control"
                                     id="last_name"
                                 />
-                            </label>
-                        </div>
-                        <div className="form-group my-2">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="text"
-                                name="username"
-                                className="form-control"
-                                id="email"
-                                // placeholder="mail@mail.fr"
-                            />
-                        </div>
-                        <div className="form-group my-2">
-                            <label htmlFor="password">Mot de passe</label>
-                            <input
-                                type="password"
-                                name="password"
-                                className="form-control"
-                                id="password"
-                                // placeholder="Mot de passe"
-                            />
-                        </div>
-                        <button type="submit" className="btn btn-outline-primary">
-                            Se connecter
-                        </button>
-                    </fieldset>
+                            </div>
+                            <div className="form-group my-2">
+                                <label htmlFor="email">Email :</label>
+                                <input
+                                    type="text"
+                                    name="email"
+                                    className="form-control"
+                                    id="email"
+                                    // placeholder="mail@mail.fr"
+                                />
+                            </div>
+                            <div className="form-group my-2">
+                                <label htmlFor="password">Mot de passe :</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    className="form-control"
+                                    id="password"
+                                    // placeholder="Mot de passe"
+                                />
+                            </div>
+                            <button type="submit" className="btn btn-outline-primary">
+                                S'enregistrer
+                            </button>
+                        </fieldset>
                 </form>
             </div>
     );
