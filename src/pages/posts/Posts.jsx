91436f51bd47aef453fb4fc904c75ceaed716736
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-01-24 22:46:03 +0100

    Display all posts from api call

--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -1,82 +1,132 @@
-import React from 'react';
+// import React from 'react';
+//
+// const Posts = (props) => {
+//     return (
+//         <div className="card-container mt-2">
+//             <div className="card border-primary mb-2">
+//                 <div className="card-header">Post name</div>
+//                 <div className="card-body">
+//                     <p className="card-text">Post Content</p>
+//                 </div>
+//             </div>
+//             <div className="card border-primary mb-2">
+//                 <div className="card-header">Post name</div>
+//                 <div className="card-body">
+//                     <p className="card-text">Post Content</p>
+//                 </div>
+//             </div>
+//             <div className="card border-primary mb-2">
+//                 <div className="card-header">Post name</div>
+//                 <div className="card-body">
+//                     <p className="card-text">Post Content</p>
+//                 </div>
+//             </div>
+//             <div className="card border-primary mb-2">
+//                 <div className="card-header">Post name</div>
+//                 <div className="card-body">
+//                     <p className="card-text">Post Content</p>
+//                 </div>
+//             </div>
+//             <div className="card border-primary mb-2">
+//                 <div className="card-header">Post name</div>
+//                 <div className="card-body">
+//                     <p className="card-text">Post Content</p>
+//                 </div>
+//             </div>
+//             <div className="card border-primary mb-2">
+//                 <div className="card-header">Post name</div>
+//                 <div className="card-body">
+//                     <p className="card-text">Post Content</p>
+//                 </div>
+//             </div>
+//             <div className="card border-primary mb-2">
+//                 <div className="card-header">Post name</div>
+//                 <div className="card-body">
+//                     <p className="card-text">Post Content</p>
+//                 </div>
+//             </div>
+//             <div className="card border-primary mb-2">
+//                 <div className="card-header">Post name</div>
+//                 <div className="card-body">
+//                     <p className="card-text">Post Content</p>
+//                 </div>
+//             </div>
+//             <div className="card border-primary mb-2">
+//                 <div className="card-header">Post name</div>
+//                 <div className="card-body">
+//                     <p className="card-text">Post Content</p>
+//                 </div>
+//             </div>
+//             <div className="card border-primary mb-2">
+//                 <div className="card-header">Post name</div>
+//                 <div className="card-body">
+//                     <p className="card-text">Post Content</p>
+//                 </div>
+//             </div>
+//             <div className="card border-primary mb-2">
+//                 <div className="card-header">Post name</div>
+//                 <div className="card-body">
+//                     <p className="card-text">Post Content</p>
+//                 </div>
+//             </div>
+//             <div className="card border-primary mb-2">
+//                 <div className="card-header">Post name</div>
+//                 <div className="card-body">
+//                     <p className="card-text">Post Content</p>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+//
+// export  default Posts;
 
-const Posts = (props) => {
-    return (
-        <div className="card-container mt-2">
-            <div className="card border-primary mb-2">
-                <div className="card-header">Post name</div>
-                <div className="card-body">
-                    <p className="card-text">Post Content</p>
-                </div>
-            </div>
-            <div className="card border-primary mb-2">
-                <div className="card-header">Post name</div>
-                <div className="card-body">
-                    <p className="card-text">Post Content</p>
-                </div>
-            </div>
-            <div className="card border-primary mb-2">
-                <div className="card-header">Post name</div>
-                <div className="card-body">
-                    <p className="card-text">Post Content</p>
-                </div>
-            </div>
-            <div className="card border-primary mb-2">
-                <div className="card-header">Post name</div>
-                <div className="card-body">
-                    <p className="card-text">Post Content</p>
-                </div>
-            </div>
-            <div className="card border-primary mb-2">
-                <div className="card-header">Post name</div>
-                <div className="card-body">
-                    <p className="card-text">Post Content</p>
-                </div>
-            </div>
-            <div className="card border-primary mb-2">
-                <div className="card-header">Post name</div>
-                <div className="card-body">
-                    <p className="card-text">Post Content</p>
-                </div>
-            </div>
-            <div className="card border-primary mb-2">
-                <div className="card-header">Post name</div>
-                <div className="card-body">
-                    <p className="card-text">Post Content</p>
-                </div>
-            </div>
-            <div className="card border-primary mb-2">
-                <div className="card-header">Post name</div>
-                <div className="card-body">
-                    <p className="card-text">Post Content</p>
-                </div>
-            </div>
-            <div className="card border-primary mb-2">
-                <div className="card-header">Post name</div>
-                <div className="card-body">
-                    <p className="card-text">Post Content</p>
-                </div>
-            </div>
-            <div className="card border-primary mb-2">
-                <div className="card-header">Post name</div>
-                <div className="card-body">
-                    <p className="card-text">Post Content</p>
-                </div>
-            </div>
-            <div className="card border-primary mb-2">
-                <div className="card-header">Post name</div>
-                <div className="card-body">
-                    <p className="card-text">Post Content</p>
-                </div>
-            </div>
-            <div className="card border-primary mb-2">
-                <div className="card-header">Post name</div>
-                <div className="card-body">
-                    <p className="card-text">Post Content</p>
-                </div>
-            </div>
-        </div>
-    );
-};
+import React, { Component } from 'react'
+import axios from 'axios'
 
-export  default Posts;
+class Posts extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/posts')
+            .then(response => {
+                console.log(response.data['hydra:member'])
+                this.setState({posts: response.data['hydra:member']})
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    render() {
+        const { posts } = this.state
+        return(
+            <div>
+                <h1>Listes des posts</h1>
+                {
+                    posts.length ?
+                        posts.map(post =>
+                            <div className="card-container mt-2" key={post.id}>
+                                <div className="card border-primary mb-2">
+                                    <div className="card-header">{post.name}</div>
+                                    <div className="card-body">
+                                        <p className="card-text">{post.content}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            // <div key={post.id}>{post.name}</div>
+                        ) :
+                        null
+                }
+            </div>
+        )
+    }
+}
+
+export default Posts
