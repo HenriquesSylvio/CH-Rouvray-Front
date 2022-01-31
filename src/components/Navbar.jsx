e0219a76e5dd6523f90dd53f29fee0d3e6f20b48
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-01-31 21:41:24 +0100

    Add modal windows for login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React, {useContext} from "react"
+import React, {useContext, useState} from "react"
 import { NavLink } from "react-router-dom";
 import Auth from "../contexts/Auth";
 import {logout} from "../services/AuthApi";
 import {toast} from "react-toastify";
+import LoginModal from "./LoginModal";
 
 const Navbar = () => {
     const {isAuthenticated, setIsAuthenticated} = useContext(Auth);
@@ -13,61 +14,77 @@ const Navbar = () => {
         toast.info('A bientôt ! 😋');
     }
 
+    const [openModal, setOpenModal] = useState(false);
+
     return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <div className="container-fluid">
-                <span className="navbar-brand">CH du Rouvray</span>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarColor01">
-                    <ul className="navbar-nav me-auto">
-                        <li className="nav-item">
-                            <NavLink className="nav-link active" to="/">
-                                Accueil
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link active" to="/posts">
-                                Posts
-                            </NavLink>
-                        </li>
-                    </ul>
-                    <ul className="navbar-nav ml-auto">
-                        {(!isAuthenticated && (
-                            <>
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container-fluid">
+                    <span className="navbar-brand">CH du Rouvray</span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarColor01">
+                        <ul className="navbar-nav me-auto">
                             <li className="nav-item">
-                                <NavLink className="nav-link active" to="/login">
-                                    Se connecter
+                                <NavLink className="nav-link active" to="/">
+                                    Accueil
                                 </NavLink>
                             </li>
+                            {(isAuthenticated && (
                             <li className="nav-item">
-                                <NavLink className="nav-link active" to="/register">
-                                    S'enregistrer
+                                <NavLink className="nav-link active" to="/posts">
+                                    Posts
                                 </NavLink>
                             </li>
-                            </>
-                        )) || (
-                            <>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link active" to="/account">
-                                        Mon compte
-                                    </NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <button className="btn btn-danger" onClick={handleLogout}>
-                                        Deconnexion
-                                    </button>
-                                </li>
-                            </>
-                        )}
-                    </ul>
+                            )) || "" }
+                        </ul>
+                        <ul className="navbar-nav ml-auto">
+                            {(!isAuthenticated && (
+                                <>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link active" to="/login">
+                                            Se connecter
+                                        </NavLink>
+                                        <button className="openModalBtn"
+                                        onClick={() => {
+                                            setOpenModal(true);
+                                        }}>
+                                            Se connecter
+                                        </button>
+                                    </li>
+
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link active" to="/register">
+                                            S'enregistrer
+                                        </NavLink>
+                                    </li>
+                                </>
+                            )) || (
+                                <>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link active" to="/account">
+                                            Mon compte
+                                        </NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <button className="btn btn-danger" onClick={handleLogout}>
+                                            Deconnexion
+                                        </button>
+                                    </li>
+                                </>
+                            )}
+                        </ul>
+                    </div>
                 </div>
+            </nav>
+            <div>
+                {openModal && <LoginModal closeModal={setOpenModal}/>}
             </div>
-        </nav>
-)
+        </div>
+    )
 }
 
 export default Navbar;
